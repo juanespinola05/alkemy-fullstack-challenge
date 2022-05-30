24b8d67d985214b01e19e4f2dea474a860bff5ca
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-05-30 18:00:37 -0300

    [API] Add error handler middlewares

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { port } = require('./config/config')
 const { setupRoutes } = require('./controllers')
+const { logErrors, handleSQLError, handleBoomError, handleError } = require('./middlewares/error.handler')
 
 const app = express()
 app.use(express.json())
@@ -14,3 +15,8 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on ${port}`)
 })
+
+app.use(logErrors)
+app.use(handleSQLError)
+app.use(handleBoomError)
+app.use(handleError)
