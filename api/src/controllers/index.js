cfaacbe1242a6e9898a0b45ea74a60cb169c425f
= <=>
2022-05-30 23:55:43 -0300

    [API] Add initial user login logic

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 
 const { usersRouter } = require('./users.controller')
+const { loginRouter } = require('./login.controller')
 
 function setupRoutes (app) {
   app.use('/api', router)
   router.use('/users', usersRouter)
+  router.use('/login', loginRouter)
 }
 
 module.exports = { setupRoutes }
---
+++ api/src/services/login.service.js
@@ -0,0 +1,45 @@
+const { models } = require('../lib/sequelize')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../config/config')
+
+class LoginService {
+  async login (data) {
+    const { email, password } = data
+    const user = await models.User.findOne({
+      where: {
+        email
+      }
+    })
+
+    const passwordMatch = user === null
+      ? false
+      : await bcrypt.compare(password, user.password)
+
+    if (!user || !passwordMatch) {
+      return {
+        message: 'Invalid email or password'
+      }
+    }
+
+    const tokenPayload = {
+      email,
+      id: user.id
+    }
+
+    const token = jwt.sign(
+      tokenPayload,
+      JWT_SECRET,
+      {
+        expiresIn: 60 * 60 * 24 * 7 * 30
+      }
+    )
+
+    return {
+      email: user.email,
+      token
+    }
+  }
+}
+
+module.exports = { LoginService }
