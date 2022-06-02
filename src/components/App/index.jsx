3c1f154b6ce6d94ac8f76280454c9365a64bc6e5
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-06-01 21:55:08 -0300

    [Web] Build initial App UI and routing

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,37 @@
-import { Titulo } from './styles'
+import { Nav } from '../'
+import { Routes, Route, Link } from 'react-router-dom'
+import { AppContainer, LinkItem } from './styles'
 
 function App () {
   return (
-    <div>
-      <Titulo>
-        T-Finance
-      </Titulo>
-    </div>
+    <AppContainer className='app'>
+      <Nav.Mobile>
+        <LinkItem>
+          <Link to='/'>
+            Home
+          </Link>
+        </LinkItem>
+        <LinkItem>
+          <Link to='/operations'>
+            Operations
+          </Link>
+        </LinkItem>
+        <LinkItem>
+          <Link to='/my-account'>
+            My account
+          </Link>
+        </LinkItem>
+      </Nav.Mobile>
+      <main>
+        <Routes>
+          <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/operations' element={<h1>Operations</h1>} />
+          <Route path='/my-account' element={<h1>My Account</h1>} />
+          <Route path='/login' element={<h1>Login</h1>} />
+          <Route path='/register' element={<h1>Register</h1>} />
+        </Routes>
+      </main>
+    </AppContainer>
   )
 }
 
--- src/components/App/styles.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components'
-
-export const Titulo = styled.h1`
-  color: #5375ff
-`
