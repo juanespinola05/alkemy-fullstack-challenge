5cffb880a92ff8e2c61b23977e00d9c8a8f9d387
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-06-03 13:02:31 -0300

    [Web] Remove card padding

--- src/components/Card/cardStyles.js
+++ src/components/Card/cardStyles.js
@@ -8,9 +8,9 @@ const variants = {
 
 export const CardContainer = styled.div`
   width: 100%;
-  padding: 15px;
   border-radius: 20px;
   background-color: var(--color-white);
   grid-area: ${({ area }) => area};
   height: ${({ variant }) => variants[variant]};
+  overflow: hidden;
 `
