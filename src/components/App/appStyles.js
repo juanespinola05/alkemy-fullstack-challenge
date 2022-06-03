d1bb57c5f7e6ea2e215cd3f71613db7fc1e5b35d
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-06-03 13:01:26 -0300

    [Web] Change general app styles

--- src/components/App/appStyles.js
+++ src/components/App/appStyles.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
+import { devices } from '../../GlobalStyles'
 
 export const AppContainer = styled.div`
-  display: grid;
+  margin-inline: auto;
   width: 100%;
+  padding-inline: 15px;
+  display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: 60px 400px repeat(2, 200px) repeat(3, 400px);
   grid-template-areas:
@@ -13,8 +16,12 @@ export const AppContainer = styled.div`
   'form form'
   'grafic grafic'
   'operations operations';
-  gap: 15px;
-  @media screen and (min-width: 1024px) {
+  gap: 30px;
+  @media (${devices.tablet}) {
+    width: 750px;
+  }
+  @media (${devices.desktop}) {
+    width: clamp(970px, 90%, 1170px);
     grid-template-columns: repeat(4, 1fr);
     grid-template-rows: 60px 300px 150px 400px;
     grid-template-areas:
@@ -22,19 +29,6 @@ export const AppContainer = styled.div`
     'total total form form'
     'small1 small2 small3 small4'
     'operations operations grafic grafic'
-  }
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
-  @media (min-width: 768px) {
-    width: 750px;
-  }
-  @media (min-width: 992px) {
-    width: 970px;
-  }
-  @media (min-width: 1200px) {
-    width: 1170px;
   }
 
 `
