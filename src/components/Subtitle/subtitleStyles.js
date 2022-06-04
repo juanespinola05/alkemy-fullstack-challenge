47801bd9b0c12da625043739d88119ae5f358715
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-06-04 01:41:11 -0300

    [Web] Change subtitle size

--- src/components/Subtitle/subtitleStyles.js
+++ src/components/Subtitle/subtitleStyles.js
@@ -6,7 +6,7 @@ const variants = {
 }
 
 export const SubtitleH2 = styled.h2`
-  font-weight: 900;
+  font-weight: 700;
   font-size: 20px;
   color: ${({ variant }) => variants[variant] ?? variants.black};
 `
