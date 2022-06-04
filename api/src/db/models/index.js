669ad43b1fd6b109c9c382f749ef1672d096ea5d
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-06-04 12:55:16 -0300

    [API] Associate new models

--- api/src/db/models/index.js
+++ api/src/db/models/index.js
@@ -1,11 +1,14 @@
 const { UserSchema, User } = require('./user.model')
 const { OperationSchema, Operation } = require('./operations.model')
+const { CategorySchema, Category } = require('./category.model')
 
 function setupModels (sequelize) {
   User.init(UserSchema, User.config(sequelize))
+  Category.init(CategorySchema, Category.config(sequelize))
   Operation.init(OperationSchema, Operation.config(sequelize))
 
   User.associate(sequelize.models)
+  Category.associate(sequelize.models)
   Operation.associate(sequelize.models)
 }
 
