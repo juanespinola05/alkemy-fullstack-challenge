a53b087c28c986b060e685b8dff5801f9afd0a20
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-06-05 10:55:05 -0300

    [API] Fix create-username migration

--- api/src/db/migrations/20220531134025-add-username-field-users.js
+++ api/src/db/migrations/20220531134025-add-username-field-users.js
@@ -1,11 +1,14 @@
 'use strict'
 
-const { USER_TABLE, UserSchema } = require('../models/user.model')
+const { USER_TABLE } = require('../models/user.model')
+const { DataTypes } = require('sequelize')
 
 module.exports = {
   async up (queryInterface) {
     await queryInterface.addColumn(USER_TABLE, 'username', {
-      ...UserSchema.username
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true
     })
   },
 
