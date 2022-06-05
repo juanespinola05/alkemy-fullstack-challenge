b748124cadefa1dbf57742be13b039765bacf04b
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-06-05 15:17:05 -0300

    [Web] Create initial input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,9 @@
+import { StyledInput } from './inputStyles.js'
+
+const Input = (props) => {
+  return (
+    <StyledInput {...props} />
+  )
+}
+
+export { Input }
---
+++ src/components/Input/inputStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const StyledInput = styled.input`
+  width: 100%;
+  height: 40px;
+  border: 1px solid var(--color-main);
+  padding: 5px;
+  font-size: 16px;
+  color: var(--color-gray-darker);
+  border-radius: 10px;
+`
